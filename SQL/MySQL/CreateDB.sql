db18864327433d2409601a9a62d5d05af29392aa
Andrés Felipe Gutiérrez Rivera <felipe.gutierrezri@gmail.com>
2025-11-06 00:48:14 -0500

    feat: Configurar zona horaria de Colombia en script MySQL
    - Agrega SET time_zone = '-05:00' después de crear la base de datos
    - Establece la zona horaria de Colombia (UTC-5) para todas las conexiones
    - Asegura consistencia en timestamps entre Django y MySQL
    - Previene problemas de conversión de fechas en producción
    
    La zona horaria coincide con la configuración de Django (America/Bogota)
    establecida en settings.py.

--- SQL/MySQL/CreateDB.sql
+++ SQL/MySQL/CreateDB.sql
@@ -4,6 +4,7 @@
 
 -- CREAR LA BASE DE DATOS
 CREATE DATABASE IF NOT EXISTS `proyecto` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+SET time_zone = '-05:00';
 
 -- SELECCIONAR LA BASE DE DATOS
 USE `proyecto`;
